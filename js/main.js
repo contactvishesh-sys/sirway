4420b42300095e742e0646b921fd603c7a0f5e6f
contactvishesh-sys <contactvishesh@gmail.com>
2026-06-29 06:18:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -390,3 +390,32 @@ toast.classList.remove("show");
 
 }
 console.log("SirWay v1 Loaded Successfully");
+
+/*=====================================
+STEP 7 - PHASE 3
+SCROLL REVEAL
+=====================================*/
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+
+    const windowHeight = window.innerHeight;
+
+    reveals.forEach(item=>{
+
+        const top = item.getBoundingClientRect().top;
+
+        if(top < windowHeight - 120){
+
+            item.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll",revealOnScroll);
+
+window.addEventListener("load",revealOnScroll);
